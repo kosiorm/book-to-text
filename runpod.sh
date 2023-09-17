86382ff0c9250304032f2b5360d818f8aa1c0b82
kosiorm <kosior.mateusz88@gmail.com>
2023-09-17 09:40:39 +0200

    test

--- runpod.sh
+++ runpod.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
-
 handle_error() {
   case $1 in
-    20) echo "Error occurred while updating package lists.";;
-    22) echo "Error occurred while installing FFmpeg.";;
-    18) echo "Error occurred while installing curl.";;
-    38) echo "Error occurred while installing project dependencies.";;
-    41) echo "Error occurred while downloading the Miniconda installation script.";;
-    43) echo "Error occurred while running the Miniconda installation script.";;
-    45) echo "Error occurred while removing the Miniconda installation script.";;
-    48) echo "Error occurred while adding Conda to PATH.";;
-    50) echo "Error occurred while activating the Conda base environment.";;
-    52) echo "Error occurred while updating Conda.";;
-    54) echo "Error occurred while creating a Python 3.10 environment.";;
-    56) echo "Error occurred while activating the Python environment.";;
-    58) echo "Error occurred while installing PyTorch 2.0, Torchaudio 2.0, and PyTorch CUDA 11.8.";;
-    60) echo "Error occurred while installing whisperx.";;
-    61) echo "Error occurred while installing Node.js and npm.";;
-
+    10) echo "Error occurred while updating package lists.";;
+    20) echo "Error occurred while installing FFmpeg.";;
+    30) echo "Error occurred while installing curl.";;
+    40) echo "Error occurred while installing project dependencies.";;
+    50) echo "Error occurred while downloading the Miniconda installation script.";;
+    60) echo "Error occurred while running the Miniconda installation script.";;
+    70) echo "Error occurred while removing the Miniconda installation script.";;
+    80) echo "Error occurred while adding Conda to PATH.";;
+    90) echo "Error occurred while activating the Conda base environment.";;
+    100) echo "Error occurred while updating Conda.";;
+    110) echo "Error occurred while creating a Python 3.10 environment.";;
+    120) echo "Error occurred while activating the Python environment.";;
+    130) echo "Error occurred while installing PyTorch 2.0, Torchaudio 2.0, and PyTorch CUDA 11.8.";;
+    140) echo "Error occurred while installing whisperx.";;
+    150) echo "Error occurred while installing Node.js and npm.";;
+    160) echo "Error occurred while installing Wine.";;
+    *) echo "An unknown error occurred.";;
   esac
   exit $1
 }
 
-apt-get update || handle_error 20
+apt-get update || handle_error 10
 
-apt-get install -y ffmpeg || handle_error 22
+apt-get install -y ffmpeg || handle_error 20
 
-apt-get install -y curl || handle_error 18
+apt-get install -y curl || handle_error 30
 
 apt-get install -y libatk1.0-0
 
@@ -41,34 +41,34 @@ apt-get install -y libxcomposite1
 
 apt-get install -y libxdamage1
 
+apt-get install -y wine || handle_error 160
 
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash || handle_error 61
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash || handle_error 150
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-nvm install --lts || handle_error 61
+nvm install --lts || handle_error 150
 
-npm install || handle_error 38
+npm install || handle_error 40
 
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh || handle_error 41
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh || handle_error 50
 
-bash Miniconda3-latest-Linux-x86_64.sh -b -u -p $HOME/miniconda || handle_error 43
+bash Miniconda3-latest-Linux-x86_64.sh -b -u -p $HOME/miniconda || handle_error 60
 
-rm Miniconda3-latest-Linux-x86_64.sh || handle_error 45
+rm Miniconda3-latest-Linux-x86_64.sh || handle_error 70
 
 echo 'export PATH="$HOME/miniconda/bin:$PATH"' >> ~/.bashrc
-. ~/.bashrc || handle_error 48
+. ~/.bashrc || handle_error 80
 
-source $HOME/miniconda/bin/activate || handle_error 50
+source $HOME/miniconda/bin/activate || handle_error 90
 
-conda update -n base -c defaults conda -y || handle_error 52
+conda update -n base -c defaults conda -y || handle_error 100
 
-conda create -n btt python=3.10 -y || handle_error 54
+conda create -n btt python=3.10 -y || handle_error 110
 
-conda activate btt || handle_error 56
+conda activate btt || handle_error 120
 
-conda install -c pytorch -c nvidia pytorch=2.0.0 torchaudio=2.0.0 torchvision -y || handle_error 58
+conda install -c pytorch -c nvidia pytorch=2.0.0 torchaudio=2.0.0 torchvision -y || handle_error 130
 
-pip install git+https://github.com/m-bain/whisperx.git || handle_error 60
+pip install git+https://github.com/m-bain/whisperx.git || handle_error 140
